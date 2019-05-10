62cb7e8bb5695b0314d3c036530d0eae8b369457
CK Hu <ck.hu@mediatek.com>
2019-05-10 16:47:15 +0800

    drm/mediatek: Separate mtk_hdmi_phy to an independent module
    mtk_hdmi_phy is a part of mtk_hdmi module, but phy driver should be an
    independent module rather than be part of drm module, so separate the phy
    driver to an independent module.
    
    Signed-off-by: CK Hu <ck.hu@mediatek.com>

--- drivers/gpu/drm/mediatek/mtk_hdmi.c
+++ drivers/gpu/drm/mediatek/mtk_hdmi.c
@@ -1786,7 +1786,6 @@ static struct platform_driver mtk_hdmi_driver = {
 };
 
 static struct platform_driver * const mtk_hdmi_drivers[] = {
-	&mtk_hdmi_phy_driver,
 	&mtk_hdmi_ddc_driver,
 	&mtk_cec_driver,
 	&mtk_hdmi_driver,
